77796089079cb8d7943bc7311af6ab7cc1589e78
Dwayne <dune.njenga@student.moringaschool.com>
2025-03-06 09:02:08 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+
+const leastFavouriteCustomer = "Alex"
+
+function changeLeastFavoriteCustomer(){
+    leastFavouriteCustomer = "Mercy";
+    return leastFavouriteCustomer;
+}
